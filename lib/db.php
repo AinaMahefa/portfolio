cfba603c09fad72d6217d67be5de5efa188b52de
AinaMahefa <jaryainamahefa@gmail.com>
2023-03-31 10:31:05 +0200

    secrets
    andrana

--- lib/db.php
+++ lib/db.php
@@ -1,6 +1,6 @@
 <?php 
 try {
-	$db = new PDO('mysql:host=localhost;dbname=portfolio','root','');
+	$db = new PDO('mysql:host=localhost;dbname=portfolio','root','${{secrets.MON_SECRET}}');
 	$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 } catch (Exception $e) {
