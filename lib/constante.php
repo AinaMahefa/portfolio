54138ce1c7fdeb252b0fe74e1ddf8fecfe805e5b
AinaMahefa <jaryainamahefa@gmail.com>
2023-03-31 10:32:04 +0200

    Update constante.php
    chemain

--- lib/constante.php
+++ lib/constante.php
@@ -1,9 +1,9 @@
 <?php
-define('WEBROOT', dirname($_SERVER['SCRIPT_NAME']).'/');
+//define('WEBROOT', dirname($_SERVER['SCRIPT_NAME']).'/');
 define('WWW_ROOT', dirname(dirname(__FILE__)));
-/**
-$directory = basename(dirname(dirname(__FILE__)));
-$url = explore($directory, $_SERVER['REQUEST_URI']);
+
+$directory = basename(WWW_ROOT);
+$url = explode($directory, $_SERVER['REQUEST_URI']);
 
 if(count($url) == 1){
 	define('WEBROOT', '/');
@@ -12,6 +12,9 @@ if(count($url) == 1){
 	define('WEBROOT',$url[0]. 'portfolio/');
 	
 }
-**/
+
 
 define('IMAGES', WWW_ROOT.DIRECTORY_SEPARATOR.'images'.DIRECTORY_SEPARATOR);
+define('IMGHOME', WEBROOT.'images/');
+
+define('CSS', WEBROOT.'assets/');
